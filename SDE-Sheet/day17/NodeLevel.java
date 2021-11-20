9ef9fbc7fd4c09930b88f5bc21819296822a941b
Chaitanya <guptachaitanya31@gmail.com>
2021-11-21 01:01:27 +0530

    day17

---
+++ SDE-Sheet/day17/NodeLevel.java
@@ -0,0 +1,11 @@
+package day17;
+
+class NodeLevel {
+    Node node;
+    int level;
+
+    public NodeLevel(Node node, int level) {
+        this.node = node;
+        this.level = level;
+    }
+}
