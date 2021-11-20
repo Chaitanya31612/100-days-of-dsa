15052875dcb279573aa1f5178e948514fe58b23a
Chaitanya <guptachaitanya31@gmail.com>
2021-11-20 23:41:22 +0530

    day17

---
+++ SDE-Sheet/day17/LeftView.java
@@ -0,0 +1,26 @@
+package day17;
+
+import java.util.ArrayList;
+
+public class LeftView {
+    int maxlevel = -1;
+    //Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        leftViewUtil(root, 0, list);
+        return list;
+    }
+
+    void leftViewUtil(Node root, int level, ArrayList<Integer> list) {
+        if(root == null) return;
+
+        if(level > maxlevel) {
+            list.add(root.data);
+            maxlevel = level;
+        }
+
+        leftViewUtil(root.left, level + 1, list);
+        leftViewUtil(root.right, level+1, list);
+    }
+}
