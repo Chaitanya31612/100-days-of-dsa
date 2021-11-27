1d17ed6c290e3863a11e4de1635880f1b1b3a901
Chaitanya <guptachaitanya31@gmail.com>
2021-11-27 16:23:57 +0530

    day21

--- SDE-Sheet/day20/ValidateBinaryTree.java
+++ SDE-Sheet/day20/ValidateBinaryTree.java
@@ -3,14 +3,28 @@ package day20;
 import day17.TreeNode;
 
 public class ValidateBinaryTree {
+    // optimal with range
+
     public boolean isValidBST(TreeNode root) {
+        return isValidBSTUtil(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBSTUtil(TreeNode root, long minRange, long maxRange) {
+        if(root == null) return true;
+
+        if(root.val <= minRange || root.val >= maxRange) return false;
+
+        return isValidBSTUtil(root.left, minRange, root.val) && isValidBSTUtil(root.right, root.val, maxRange);
+    }
+
+    public boolean isValidBSTBrute(TreeNode root) {
         if(root == null) return true;
 
         // if(root.left == null && root.right == null)
         //     return true;
 
-        boolean leftBST = isValidBST(root.left);
-        boolean rightBST = isValidBST(root.right);
+        boolean leftBST = isValidBSTBrute(root.left);
+        boolean rightBST = isValidBSTBrute(root.right);
 
         if(!leftBST || !rightBST)
             return false;
