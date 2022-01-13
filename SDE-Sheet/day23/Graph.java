2f9c40d657ac103f805174d2353df167cd20c46c
Chaitanya <guptachaitanya31@gmail.com>
2022-01-13 18:20:15 +0530

    recursion and dp

--- SDE-Sheet/day23/Graph.java
+++ SDE-Sheet/day23/Graph.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Graph {
-    int v, e;
+    public int v, e;
     public ArrayList<ArrayList<Integer>> input() {
         Scanner sc = new Scanner(System.in);
 
@@ -41,6 +41,28 @@ public class Graph {
             int sv = sc.nextInt();
 
             graph.get(fv).add(sv);
+        }
+
+        return graph;
+    }
+
+    public int[][] inputarray() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter vertices and edges");
+        v = sc.nextInt();
+        e = sc.nextInt();
+
+        int[][] graph = new int[v][v];
+
+        System.out.println("Enter fv, sv, wt for " + e + " times");
+        for(int i = 0; i < e; i++) {
+            int fv = sc.nextInt();
+            int sv = sc.nextInt();
+            int wt = sc.nextInt();
+
+            graph[fv][sv] = wt;
+            graph[sv][fv] = wt;
         }
 
         return graph;
